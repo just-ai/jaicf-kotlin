0ab902c634f3e77942dc0c094012bfb2fdafa234
morfeusys <che@just-ai.com>
2020-03-22 15:08:43 +0300

    SSML.kt docs

--- core/src/main/kotlin/com/justai/jaicf/helpers/ssml/SSML.kt
+++ core/src/main/kotlin/com/justai/jaicf/helpers/ssml/SSML.kt
@@ -2,6 +2,20 @@ package com.justai.jaicf.helpers.ssml
 
 import com.justai.jaicf.helpers.http.toUrl
 
+/**
+ * SSML (Speech Synthesis Markup Language) helper functions allow to add SSML tags to the string output.
+ * SSML is supported by some voice-enabled channels like Alexa and Google Actions.
+ *
+ * Usage example:
+ *
+ * ```
+ *  action {
+ *    reactions.say("Hello $break200ms there! You're the ${ordinal(100)} user.")
+ *    reactions.say("Just listen this. ${audio("https://someaddress.com/audio.mp3")}")
+ *  }
+ * ```
+ */
+
 val break200ms = breakMs(200)
 val break300ms = breakMs(300)
 val break500ms = breakMs(500)
