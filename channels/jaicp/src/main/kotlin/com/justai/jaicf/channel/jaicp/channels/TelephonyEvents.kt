a2c0a33073c6ead432c884616e99fe5a9e8a33e5
Denire <denisire@gmail.com>
2020-04-03 19:28:27 +0300

    add javadoc for TelephonyEvents.kt [skip ci]

--- channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/channels/TelephonyEvents.kt
+++ channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/channels/TelephonyEvents.kt
@@ -1,5 +1,15 @@
 package com.justai.jaicf.channel.jaicp.channels
 
+/**
+ * Events which can be send with JaicpBotRequest in Telephony channel
+ *
+ * speechNotRecognized  - is sent when ASR provider could not detect speech.
+ * hangup               - is sent when client hanged up.
+ * ringing              - is sent when we have received incoming call or started outgoing call, but client hadn't yet answered.
+ * noDmtfAnswer         - is sent when we expect client to dial specific code on keypad, but client dialed nothing.
+ *
+ * @see com.justai.jaicf.channel.jaicp.JaicpEvents
+ * */
 object TelephonyEvents {
     const val speechNotRecognized = "speechNotRecognized"
     const val hangup = "hangup"
