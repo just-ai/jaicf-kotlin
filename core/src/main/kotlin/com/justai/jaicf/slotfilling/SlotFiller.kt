38586c12eb9f761d7d6730cdf19de06a5ea747e5
Denire <denisire@gmail.com>
2020-07-26 00:55:20 +0300

    add javadocs for slotfilling

--- core/src/main/kotlin/com/justai/jaicf/slotfilling/SlotFiller.kt
+++ core/src/main/kotlin/com/justai/jaicf/slotfilling/SlotFiller.kt
@@ -2,16 +2,32 @@ package com.justai.jaicf.slotfilling
 
 import com.justai.jaicf.context.BotContext
 import com.justai.jaicf.reactions.Reactions
+import com.justai.jaicf.BotEngine
 
 /**
- * JAVADOC ME*/
+ * This interface can be implemented to create a special way to handle filling some slots.
+ * It can be useful, for example, if you want to create channel-specific slot filling,
+ * like using simple text question in messenger and pre-recorded .wav questions in telephony channel.
+ *
+ * @see [BotEngine]
+ *
+ * */
 interface SlotFiller {
 
     /**
-     * JAVADOC ME*/
+     * Tells if it's possible to fill custom this slot with your own code.
+     *
+     * @param name - name of slot to be filled
+     * @return true if can handle, no if can not
+     * */
     fun canHandle(name: String): Boolean
 
     /**
-     * JAVADOC ME*/
+     * Handles process of filling specific slot using current request's channel reactions
+     *
+     * @param name - name of slot to handle
+     * @param botContext current user's [BotContext]
+     * @param reactions current user's request [Reactions]
+     * */
     fun handle(name: String, botContext: BotContext, reactions: Reactions)
 }
--- core/src/main/kotlin/com/justai/jaicf/slotfilling/SlotFillingResult.kt
+++ core/src/main/kotlin/com/justai/jaicf/slotfilling/SlotFillingResult.kt
@@ -5,7 +5,7 @@ import com.justai.jaicf.context.ActivatorContext
 import com.justai.jaicf.reactions.Reactions
 
 /**
- * Set of available status for slotfilling.
+ * Set of available results of slotfilling processing.
  *
  * @see SlotFillingSkipped
  * @see SlotFillingFinished
@@ -15,22 +15,35 @@ import com.justai.jaicf.reactions.Reactions
 sealed class SlotFillingResult
 
 /**
- * JAVADOC ME
+ * This result indicates that either intent has no slots, or no prompts, or activator can not process it.
+ *
+ * @see [com.justai.jaicf.activator.Activator]
  * */
 object SlotFillingSkipped : SlotFillingResult()
 
 /**
- * JAVADOC ME
+ * This result indicates that intent started processing slotfilling,
+ * prompted question to client and will prompt question for another slot.
+ * 
+ * @see [com.justai.jaicf.activator.Activator]
  * */
 object SlotFillingInProgress : SlotFillingResult()
 
 /**
- * JAVADOC ME
+ * This result indicates that slot filling is interrupted by activator-specific reasons.
+ * If slot filling is interrupted, last client query will be processed in BotEngine.
+ *
+ * For example, SlotFillingInterrupted can be returned when client tried to fill slots too many times and failed it.
+ *
+ * @see [com.justai.jaicf.activator.Activator]
  * */
 object SlotFillingInterrupted : SlotFillingResult()
 
 /**
- * JAVADOC ME
+ * This result indicates that slot filling session is finished and returns activator context,
+ * which will be passed to scenario.
+ * 
+ * @see [com.justai.jaicf.activator.Activator]
  * */
 data class SlotFillingFinished(
     val activatorContext: ActivatorContext
