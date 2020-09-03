66a1b568d6ddca7b3f5b3438c65dd6f190957157
Denire <denisire@gmail.com>
2020-09-03 14:25:50 +0300

    add helper to set extension property without backig field

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("javax.servlet:javax.servlet-api" version {javaxServletApi})
     implementation("io.ktor:ktor-server-core" version {ktor})
     implementation("org.junit.jupiter:junit-jupiter-api" version {jUnit})
+    implementation("com.github.h0tk3y:kotlin-fun:v0.9")
 
     testRuntime("org.junit.jupiter:junit-jupiter-engine" version {jUnit})
 }
