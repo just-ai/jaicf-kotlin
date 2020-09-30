de08201baa4fc3bffc05927137dc91be3ee3b8ec
Denire <denisire@gmail.com>
2020-09-30 18:44:26 +0300

    [#49] remove exception throwing in BotEngine, add tests for ActionErrorHook

---
+++ core/src/test/kotlin/com/justai/jaicf/core/test/hooks/ActionErrorHookHandlingTest.kt
@@ -0,0 +1,44 @@
+package com.justai.jaicf.core.test.hooks
+
+import com.justai.jaicf.hook.ActionErrorHook
+import com.justai.jaicf.model.scenario.Scenario
+import com.justai.jaicf.test.ScenarioTest
+import org.junit.jupiter.api.Test
+
+val scenarioWithErrorHook = object : Scenario() {
+    init {
+        handle<ActionErrorHook> {
+            it.reactions.say("Error happened. Sorry.")
+        }
+
+        state("error") {
+            activators { regex("error") }
+            action {
+                error("This is error")
+            }
+        }
+
+        state("ok") {
+            activators { regex("ok") }
+            action {
+                reactions.say("ok")
+            }
+        }
+    }
+}
+
+class ActionErrorHookHandlingTest : ScenarioTest(scenarioWithErrorHook.model) {
+
+    @Test
+    fun `should handle error with some reaction`() {
+        query("error") responds "Error happened. Sorry."
+    }
+
+    @Test
+    fun `should not invoke error hook when no errors`() {
+        query("ok") responds "ok"
+    }
+
+}
+
+
