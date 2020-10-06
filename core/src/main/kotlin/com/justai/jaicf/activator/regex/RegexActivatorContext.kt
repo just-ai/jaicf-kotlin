aa67f8d87594e91cac481f24b5f21b3d9357e899
morfeusys <che@just-ai.com>
2020-10-06 17:40:31 +0300

    RegexActivator without reflection

--- core/src/main/kotlin/com/justai/jaicf/activator/regex/RegexActivatorContext.kt
+++ core/src/main/kotlin/com/justai/jaicf/activator/regex/RegexActivatorContext.kt
@@ -2,22 +2,22 @@ package com.justai.jaicf.activator.regex
 
 import com.justai.jaicf.context.ActivatorContext
 import com.justai.jaicf.context.StrictActivatorContext
+import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 /**
  * Appears in the context of action block if [RegexActivator] handled the user's request.
  *
  * @property pattern a regex pattern that activated a state
- * @property groups a list of groups fetched from the request's text
- * @property namedGroups a map of named groups fetched from the request's text
+ * @property matcher a matching result, contains matched groups that can be retrieved by index or name
+ *
+ * @see Pattern
+ * @see Matcher
  */
 data class RegexActivatorContext(
-    val pattern: Pattern
-): StrictActivatorContext() {
-
-    val groups = mutableListOf<String?>()
-    val namedGroups = mutableMapOf<String, String?>()
-}
+    val pattern: Pattern,
+    val matcher: Matcher
+): StrictActivatorContext()
 
 val ActivatorContext.regex
     get() = this as? RegexActivatorContext
