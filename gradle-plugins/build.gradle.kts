4016324d3cd57f9961f83e170c0b7da141ba4517
Nikita Voloshin <n.voloshin@just-ai.com>
2020-10-21 16:01:34 +0300

    Setup bintray upload

--- gradle-plugins/build.gradle.kts
+++ gradle-plugins/build.gradle.kts
@@ -1,24 +1,25 @@
 buildscript {
     repositories {
-        google()
         jcenter()
-        maven("https://plugins.gradle.org/m2/")
     }
 
     dependencies {
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.0")
     }
 }
 
-allprojects {
-
+subprojects {
     group = "com.justai.jaicf"
 
     repositories {
+        google()
         jcenter()
         mavenCentral()
         mavenLocal()
         maven(uri("https://jitpack.io"))
+    }
+
+    afterEvaluate {
+        apply(from = rootProject.file("release/bintray.gradle"))
     }
 }
