914c43e05e1bf95c5ad4ef5227e4b74e3dd38b1f
morfeusys <che@just-ai.com>
2020-11-02 12:24:25 +0300

    Telephony example improvements

--- examples/jaicp-telephony/src/main/kotlin/com/justai/jaicf/examples/jaicptelephony/TelephonyBot.kt
+++ examples/jaicp-telephony/src/main/kotlin/com/justai/jaicf/examples/jaicptelephony/TelephonyBot.kt
@@ -2,8 +2,6 @@ package com.justai.jaicf.examples.jaicptelephony
 
 import com.justai.jaicf.BotEngine
 import com.justai.jaicf.activator.caila.CailaIntentActivator
-import com.justai.jaicf.activator.catchall.CatchAllActivator
-import com.justai.jaicf.activator.event.BaseEventActivator
 import com.justai.jaicf.activator.regex.RegexActivator
 import com.justai.jaicf.channel.jaicp.logging.JaicpConversationLogger
 
@@ -12,10 +10,8 @@ val cailaIntentActivator = CailaIntentActivator.Factory(nluSettings)
 val telephonyCallScenario = BotEngine(
     TelephonyBotScenario.model,
     activators = arrayOf(
-        BaseEventActivator,
-        RegexActivator,
         cailaIntentActivator,
-        CatchAllActivator
+        RegexActivator
     ),
     conversationLoggers = arrayOf(JaicpConversationLogger(accessToken))
 )
