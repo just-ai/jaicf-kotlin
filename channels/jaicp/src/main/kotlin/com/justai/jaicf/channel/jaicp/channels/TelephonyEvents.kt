19d7e39a1c56847a919e90e21bf6b30eac56b570
Denire <denisire@gmail.com>
2020-11-16 22:31:15 +0300

    telephony events improvements

--- channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/channels/TelephonyEvents.kt
+++ channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/channels/TelephonyEvents.kt
@@ -5,6 +5,7 @@ package com.justai.jaicf.channel.jaicp.channels
  *
  * SPEECH_NOT_RECOGNISED    - is sent when ASR provider could not detect speech.
  * HANGUP                   - is sent when client hanged up.
+ * BOT_HANGUP               - is sent when bot hanged up.
  * RINGING                  - is sent when we have received incoming call or started outgoing call, but client hadn't yet answered.
  * NO_DTMF_ANSWER           - is sent when we expect client to dial specific code on keypad, but client dialed nothing.
  *
@@ -12,7 +13,8 @@ package com.justai.jaicf.channel.jaicp.channels
  * */
 object TelephonyEvents {
     const val SPEECH_NOT_RECOGNISED = "speechNotRecognized"
-    const val HANGUP = "botHangup"
+    const val BOT_HANGUP = "botHangup"
+    const val HANGUP = "hangup"
     const val RINGING = "ringing"
     const val NO_DTMF_ANSWER = "noDtmfAnswerEvent"
 }
