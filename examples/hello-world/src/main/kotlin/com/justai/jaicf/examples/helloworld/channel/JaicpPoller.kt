fa91ca48c9c25ff950f09202242c25454270f81d
morfeusys <che@just-ai.com>
2020-12-02 19:46:35 +0300

    example fix

--- examples/hello-world/src/main/kotlin/com/justai/jaicf/examples/helloworld/channel/JaicpPoller.kt
+++ examples/hello-world/src/main/kotlin/com/justai/jaicf/examples/helloworld/channel/JaicpPoller.kt
@@ -24,7 +24,7 @@ fun main() {
                 FacebookChannel,
                 AimyboxChannel,
                 AlexaChannel,
-                ActionsFulfillmentDialogflow
+                ActionsFulfillmentDialogflow()
             )
         ).runBlocking()
     }
