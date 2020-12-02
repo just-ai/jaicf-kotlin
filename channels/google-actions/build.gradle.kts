66568d6d2d89afb9f85d7538ce8e78c8f0bc448b
morfeusys <che@just-ai.com>
2020-12-02 16:56:57 +0300

    in progress

--- channels/google-actions/build.gradle.kts
+++ channels/google-actions/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     implementation(project(":core"))
     implementation(kotlin("stdlib", Version.stdLib))
 
+    api("com.fasterxml.jackson.module:jackson-module-kotlin" version {jackson})
     api("com.google.actions:actions-on-google:1.8.0")
 }
 
---
+++ channels/google-actions/src/main/kotlin/com/justai/jaicf/channel/googleactions/manager/ActionsBotContextManager.kt
@@ -0,0 +1,17 @@
+package com.justai.jaicf.channel.googleactions.manager
+
+import com.justai.jaicf.api.BotRequest
+import com.justai.jaicf.api.BotResponse
+import com.justai.jaicf.context.BotContext
+import com.justai.jaicf.context.manager.BotContextManager
+
+class ActionsBotContextManager: BotContextManager {
+
+    override fun loadContext(request: BotRequest): BotContext {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveContext(botContext: BotContext, request: BotRequest?, response: BotResponse?) {
+        TODO("Not yet implemented")
+    }
+}
