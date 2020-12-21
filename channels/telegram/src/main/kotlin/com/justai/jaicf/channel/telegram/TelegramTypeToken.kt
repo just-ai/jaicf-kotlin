9c07e4439b2af58ba5c7946013ba3bf0a78978f2
Nikita Voloshin <n.voloshin@just-ai.com>
2020-12-21 20:03:33 +0300

    Add type tokens for telegram request types

--- channels/telegram/src/main/kotlin/com/justai/jaicf/channel/telegram/TelegramTypeToken.kt
+++ channels/telegram/src/main/kotlin/com/justai/jaicf/channel/telegram/TelegramTypeToken.kt
@@ -6,5 +6,16 @@ typealias TelegramTypeToken = ChannelTypeToken<TelegramBotRequest, TelegramReact
 
 val telegram: TelegramTypeToken = ChannelTypeToken()
 
+val TelegramTypeToken.text get() = ChannelTypeToken<TelegramTextRequest, TelegramReactions>()
+val TelegramTypeToken.callback get() = ChannelTypeToken<TelegramQueryRequest, TelegramReactions>()
 val TelegramTypeToken.location get() = ChannelTypeToken<TelegramLocationRequest, TelegramReactions>()
 val TelegramTypeToken.contact get() = ChannelTypeToken<TelegramContactRequest, TelegramReactions>()
+val TelegramTypeToken.audio get() = ChannelTypeToken<TelegramAudioRequest, TelegramReactions>()
+val TelegramTypeToken.document get() = ChannelTypeToken<TelegramDocumentRequest, TelegramReactions>()
+val TelegramTypeToken.animation get() = ChannelTypeToken<TelegramAnimationRequest, TelegramReactions>()
+val TelegramTypeToken.game get() = ChannelTypeToken<TelegramGameRequest, TelegramReactions>()
+val TelegramTypeToken.photos get() = ChannelTypeToken<TelegramPhotosRequest, TelegramReactions>()
+val TelegramTypeToken.sticker get() = ChannelTypeToken<TelegramStickerRequest, TelegramReactions>()
+val TelegramTypeToken.video get() = ChannelTypeToken<TelegramVideoRequest, TelegramReactions>()
+val TelegramTypeToken.videoNote get() = ChannelTypeToken<TelegramVideoNoteRequest, TelegramReactions>()
+val TelegramTypeToken.voice get() = ChannelTypeToken<TelegramVoiceRequest, TelegramReactions>()
