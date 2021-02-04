da3eeb3bb4522bf021827c6458b4376c4f2bdf7c
Nikita Voloshin <n.voloshin@just-ai.com>
2021-02-04 10:48:28 +0300

    Cleanup root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,5 @@
-val coreProject: Project
-    get() = rootProject.project("core")
-
 plugins {
     id("com.justai.jaicf.plugins.internal.github")
-}
-
-buildscript {
-    repositories {
-        google()
-        jcenter()
-        maven("https://plugins.gradle.org/m2/")
-    }
-
-    dependencies {
-        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
-        classpath("com.github.breadmoirai:github-release:2.2.12")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.0")
-    }
 }
 
 allprojects {
