4653aa7546281af9cb6c6b1b77c73ed2049aefca
Denire <denisire@gmail.com>
2021-02-11 14:55:04 +0300

    jaicp-livechat-adapter: add switch to chatApi reactions

--- channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/reactions/ChatApiReactions.kt
+++ channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/reactions/ChatApiReactions.kt
@@ -1,8 +1,9 @@
 package com.justai.jaicf.channel.jaicp.reactions
 
 import com.justai.jaicf.reactions.Reactions
+import com.justai.jaicf.reactions.jaicp.JaicpCompatibleAsyncReactions
 
 val Reactions.chatapi
     get() = this as? ChatApiReactions
 
-class ChatApiReactions : JaicpReactions()
+class ChatApiReactions : JaicpReactions(), JaicpCompatibleAsyncReactions
