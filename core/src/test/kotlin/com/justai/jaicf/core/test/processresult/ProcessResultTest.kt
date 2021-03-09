bd2d8c68ec63915a7b3c4f34398b866270eb49e5
Denire <denisire@gmail.com>
2021-03-09 20:47:20 +0300

    add more tests, add default reactions registration for TestReactions.kt

--- core/src/test/kotlin/com/justai/jaicf/core/test/processresult/ProcessResultTest.kt
+++
@@ -1,3 +0,0 @@
-package com.justai.jaicf.core.test.processresult
-
-// TODO: Add process result tests AS MANY AS YOU CAN!!!
---
+++ core/src/test/kotlin/com/justai/jaicf/core/test/processresult/StateAssertionTest.kt
@@ -0,0 +1,64 @@
+package com.justai.jaicf.core.test.processresult
+
+import com.justai.jaicf.model.scenario.Scenario
+import com.justai.jaicf.test.ScenarioTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+private val stateAssertionScenario = object : Scenario() {
+    init {
+        state("first") {
+            activators {
+                regex("first")
+            }
+            action {
+                reactions.go("/middle")
+            }
+        }
+
+        state("middle") {
+            action {
+                reactions.go("/last")
+            }
+        }
+
+        state("last") {
+            action {
+                reactions.say("i'm in last")
+            }
+        }
+    }
+}
+
+class StateAssertionTest : ScenarioTest(stateAssertionScenario) {
+
+    @Test
+    fun `should assert all states through scenario -- positive test`() {
+        query("first") startsWithContext "/" goesToState "/first" visitsState "/middle" endsWithState "/last"
+    }
+
+    @Test
+    fun `should assert all states through scenario -- negative test`() {
+        val processResult = query("first")
+        assertThrows<AssertionError> {
+            processResult startsWithContext "/not ok"
+        }
+
+        assertThrows<AssertionError> {
+            processResult goesToState "/not ok"
+        }
+
+        assertThrows<AssertionError> {
+            processResult visitsState "/not ok"
+        }
+
+        assertThrows<AssertionError> {
+            processResult endsWithState "/not ok"
+        }
+    }
+
+    @Test
+    fun `should assert all visited states through scenario -- positive test`() {
+        query("first") visitsState "/first" visitsState "/middle" visitsState "/last"
+    }
+}
