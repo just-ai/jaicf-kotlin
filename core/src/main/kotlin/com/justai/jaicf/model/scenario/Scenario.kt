2164d37cdd0eb46f74c584a180c0446694b3d93c
Denire <denisire@gmail.com>
2021-04-05 14:49:44 +0300

    add createScenario builders for interface implementation classes

--- core/src/main/kotlin/com/justai/jaicf/model/scenario/Scenario.kt
+++ core/src/main/kotlin/com/justai/jaicf/model/scenario/Scenario.kt
@@ -1,9 +1,28 @@
 package com.justai.jaicf.model.scenario
 
+import com.justai.jaicf.api.BotRequest
+import com.justai.jaicf.builder.RootBuilder
+import com.justai.jaicf.builder.ScenarioDsl
+import com.justai.jaicf.generic.ChannelTypeToken
+import com.justai.jaicf.model.ScenarioModelBuilder
+import com.justai.jaicf.reactions.Reactions
 import kotlin.reflect.KProperty
 
 interface Scenario {
     val scenario: ScenarioModel
+
+    @ScenarioDsl
+    fun createScenario(
+        body: RootBuilder<BotRequest, Reactions>.() -> Unit
+    ): ScenarioModel = com.justai.jaicf.builder.Scenario(ChannelTypeToken.Default, body).scenario
+
+    @ScenarioDsl
+    fun <B : BotRequest, R : Reactions> createScenario(
+        channelToken: ChannelTypeToken<B, R>,
+        body: RootBuilder<B, R>.() -> Unit
+    ): ScenarioModel = object : Scenario {
+        override val scenario by lazy { RootBuilder(ScenarioModelBuilder(), channelToken).apply(body).buildScenario() }
+    }.scenario
 }
 
 operator fun Scenario.getValue(thisRef: Scenario, property: KProperty<*>): ScenarioModel = scenario
