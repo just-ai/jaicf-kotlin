b35472cdb3fbc9c92e56d4cb2d36097901ce785e
Denire <denisire@gmail.com>
2021-04-05 14:53:10 +0300

    simplify

--- core/src/main/kotlin/com/justai/jaicf/model/scenario/Scenario.kt
+++ core/src/main/kotlin/com/justai/jaicf/model/scenario/Scenario.kt
@@ -2,9 +2,9 @@ package com.justai.jaicf.model.scenario
 
 import com.justai.jaicf.api.BotRequest
 import com.justai.jaicf.builder.RootBuilder
+import com.justai.jaicf.builder.Scenario
 import com.justai.jaicf.builder.ScenarioDsl
 import com.justai.jaicf.generic.ChannelTypeToken
-import com.justai.jaicf.model.ScenarioModelBuilder
 import com.justai.jaicf.reactions.Reactions
 import kotlin.reflect.KProperty
 
@@ -14,15 +14,13 @@ interface Scenario {
     @ScenarioDsl
     fun createScenario(
         body: RootBuilder<BotRequest, Reactions>.() -> Unit
-    ): ScenarioModel = com.justai.jaicf.builder.Scenario(ChannelTypeToken.Default, body).scenario
+    ): ScenarioModel = Scenario(ChannelTypeToken.Default, body).scenario
 
     @ScenarioDsl
     fun <B : BotRequest, R : Reactions> createScenario(
         channelToken: ChannelTypeToken<B, R>,
         body: RootBuilder<B, R>.() -> Unit
-    ): ScenarioModel = object : Scenario {
-        override val scenario by lazy { RootBuilder(ScenarioModelBuilder(), channelToken).apply(body).buildScenario() }
-    }.scenario
+    ): ScenarioModel = Scenario(channelToken, body).scenario
 }
 
 operator fun Scenario.getValue(thisRef: Scenario, property: KProperty<*>): ScenarioModel = scenario
