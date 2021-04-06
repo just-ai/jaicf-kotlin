f257f56fb265fece10e59222795fbfb9ec0261c4
Denire <denisire@gmail.com>
2021-04-07 01:34:18 +0300

    add javadocs for Scenario.kt interface, simplify builders

--- core/src/main/kotlin/com/justai/jaicf/model/scenario/Scenario.kt
+++ core/src/main/kotlin/com/justai/jaicf/model/scenario/Scenario.kt
@@ -2,6 +2,25 @@ package com.justai.jaicf.model.scenario
 
 import kotlin.reflect.KProperty
 
+/**
+ * Main interface for Scenario objects.
+ *
+ * Scenario objects should be generally created by using [com.justai.jaicf.builder.Scenario] function.
+ * Alternative way for creating a scenario is by implementing this interface, see example:
+ * ```kotlin
+ * class MyScenario() : Scenario {
+ *   override val model: ScenarioModel = createModel {
+ *     // your states
+ *     }
+ * }
+ *
+ * ```
+ *
+ * @property model model of scenario
+ *
+ * @see [com.justai.jaicf.builder.Scenario]
+ * @see [com.justai.jaicf.builder.createModel]
+ * */
 interface Scenario {
     val model: ScenarioModel
 }
