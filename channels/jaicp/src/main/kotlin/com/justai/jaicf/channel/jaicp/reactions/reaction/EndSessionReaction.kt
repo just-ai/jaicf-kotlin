bc48a58a938f37ef04832e57d14cbca532bff7ab
Vep <veptechno@gmail.com>
2021-04-12 02:08:16 +0300

    add EndSessionReaction

--- channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/reactions/reaction/EndSessionReaction.kt
+++
@@ -1,9 +0,0 @@
-package com.justai.jaicf.channel.jaicp.reactions.reaction
-
-import com.justai.jaicf.logging.Reaction
-
-data class EndSessionReaction(
-    override val fromState: String
-) : Reaction(fromState) {
-    override fun toString(): String = """end current session from state $fromState"""
-}
