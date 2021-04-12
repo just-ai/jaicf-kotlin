d52563f07fb7769b67653f90dd7e686b94212292
Denire <denisire@gmail.com>
2021-04-12 19:07:25 +0300

    add bot parameter to JAICP MDC

--- channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/JaicpMDC.kt
+++ channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/JaicpMDC.kt
@@ -7,6 +7,7 @@ object JaicpMDC {
     fun setFromRequest(request: JaicpBotRequest) {
         MDC.put("requestId", request.questionId)
         MDC.put("channelId", request.channelBotId)
+        MDC.put("bot", request.channelBotId)
         MDC.put("accountId", request.channelBotId.split("-").first())
     }
 }
