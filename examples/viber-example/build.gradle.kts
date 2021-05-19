5401453fdc775f36e11573472e5594c2e67a26f1
Protasov Vitalii <veptechno@gmail.com>
2021-05-19 12:40:23 +0300

    add poller to viber example scenario

--- examples/viber-example/build.gradle.kts
+++ examples/viber-example/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation(kotlin("stdlib", Version.stdLib))
 
     implementation(project(":core"))
+    implementation(project(":channels:jaicp"))
     implementation("org.slf4j:slf4j-simple" version { slf4j })
     implementation("org.slf4j:slf4j-log4j12" version { slf4j })
     implementation("io.ktor:ktor-server-netty" version { ktor })
