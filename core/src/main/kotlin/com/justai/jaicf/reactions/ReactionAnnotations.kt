1ec9e25ae0313f06f81e919ba489c09b20b3baa5
Protasov Vitalii <veptechno@gmail.com>
2021-07-30 12:15:00 +0300

    core: add javadoc to annoataions

--- core/src/main/kotlin/com/justai/jaicf/reactions/ReactionAnnotations.kt
+++ core/src/main/kotlin/com/justai/jaicf/reactions/ReactionAnnotations.kt
@@ -1,9 +1,31 @@
 package com.justai.jaicf.reactions
 
+/**
+ * This annotation indicates to the plugin that a StatePath will be passed to the annotated parameter or receiver.
+ * This allows you to use the plugin's features, such as state navigation and inspections.
+ * To use it, you need to annotate parameters at functions or receivers or fields at primary constructors
+ *
+ * Example:
+ * ```
+ * fun @PathValue String.and(@PathValue that: String)
+ * ```
+ */
 @Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.TYPE)
 @Retention(AnnotationRetention.SOURCE)
 annotation class PathValue()
 
-@Target(AnnotationTarget.FUNCTION)
+/**
+ * This annotation indicates that the Reactions and derived classes extension function uses a reaction that must
+ * be redefined to work correctly.
+ * The plugin inspects that the method is overridden
+ *
+ * Example:
+ * ```
+ * @UsesReaction("image")
+ * fun Reactions.myImage(url: String): ImageReaction { ... }
+ * ```
+ */
 @Repeatable
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
 annotation class UsesReaction(val name: String)
