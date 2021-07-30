df81eeace5c4a1cf3bd4609aded21d69909c35b0
Protasov Vitalii <veptechno@gmail.com>
2021-07-30 05:04:03 +0300

    Multilingual bot - minor code style fixes

--- examples/multilingual-bot/src/main/kotlin/com/justai/jaicf/examples/multilingual/MultilingualBot.kt
+++ examples/multilingual-bot/src/main/kotlin/com/justai/jaicf/examples/multilingual/MultilingualBot.kt
@@ -4,7 +4,6 @@ import com.justai.jaicf.api.routing.BotRoutingEngine
 import com.justai.jaicf.examples.multilingual.bots.EnBot
 import com.justai.jaicf.examples.multilingual.bots.RuBot
 
-
 val MultilingualBotEngine = BotRoutingEngine(
     main = MainBot.engine,
     routables = mapOf(RuBot.EngineName to RuBot.Engine, EnBot.EngineName to EnBot.Engine)
