0b499983eed0767bc37d932af2035e8c26926f3c
Denire <denisire@gmail.com>
2021-07-31 14:04:33 +0300

    chat-api-reactions & response data WIP

--- channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/dto/JaicpResponseData.kt
+++ channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/dto/JaicpResponseData.kt
@@ -6,6 +6,7 @@ import com.justai.jaicf.channel.jaicp.toJson
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
 
 @SerialName("responseData")
 @Serializable
@@ -15,14 +16,16 @@ internal class JaicpResponseData private constructor(
     val dialer: JaicpDialerAPI? = null,
     val bargeIn: BargeInProperties? = null,
     val bargeInInterrupt: BargeInResponse? = null,
-    val sessionId: String
+    val sessionId: String,
+    val responseData: Map<String, JsonObject> = mapOf()
 ) {
     internal constructor(
         replies: List<Reply>,
         dialer: JaicpDialerAPI?,
         bargeInData: BargeInProperties?,
         bargeInInterrupt: BargeInResponse?,
-        sessionId: String
+        sessionId: String,
+        responseData: Map<String, JsonObject> = mapOf(),
     ) : this(
         replies = replies.map { it.serialized().toJson() },
         answer = replies.filterIsInstance<TextReply>().joinToString("\n\n") { it.text },
