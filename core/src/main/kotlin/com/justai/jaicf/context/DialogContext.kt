851668fbbc831f25716062c86f67d58aa4985771
Nikita Voloshin <n.voloshin@just-ai.com>
2021-08-05 23:20:35 +0300

    Use createTransitionHistory in constructor

--- core/src/main/kotlin/com/justai/jaicf/context/DialogContext.kt
+++ core/src/main/kotlin/com/justai/jaicf/context/DialogContext.kt
@@ -17,7 +17,7 @@ class DialogContext: Serializable {
 
     val transitions: MutableMap<String, String> = mutableMapOf()
     val backStateStack = ArrayDeque<String>()
-    var transitionHistory = ArrayDeque<String>(TRANSITION_HISTORY_SIZE_LIMIT).apply { add(currentState) }
+    var transitionHistory = createTransitionHistory()
         // don't remove, may be null
         get() = field ?: createTransitionHistory().also { field = it }
         private set
