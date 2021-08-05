578bf4c950a3c9995d203802ef5fdbf2fd66d4fa
Nikita Voloshin <n.voloshin@just-ai.com>
2021-08-05 12:53:58 +0300

    Add transitionHistory into DialogContext

--- core/src/main/kotlin/com/justai/jaicf/context/DialogContext.kt
+++ core/src/main/kotlin/com/justai/jaicf/context/DialogContext.kt
@@ -1,8 +1,13 @@
 package com.justai.jaicf.context
 
 import com.justai.jaicf.model.scenario.ScenarioModel
+import com.sun.xml.internal.bind.v2.TODO
+import java.io.Externalizable
+import java.io.ObjectInput
+import java.io.ObjectOutput
 import java.io.Serializable
 import java.util.ArrayDeque
+import java.util.Collections.addAll
 
 /**
  * Contains all data regarding the current state of the dialogue.
@@ -17,14 +22,16 @@ class DialogContext: Serializable {
 
     val transitions: MutableMap<String, String> = mutableMapOf()
     val backStateStack = ArrayDeque<String>()
+    var transitionHistory = ArrayDeque<String>(TRANSITION_HISTORY_SIZE_LIMIT).apply { add(currentState) }
+        private set
 
     fun nextState(): String? {
         currentState = nextState ?: return null
         nextState = null
-        return currentState
+        return currentState.also(::saveToTransitionHistory)
     }
 
-    fun nextContext(model: ScenarioModel): String? {
+    fun nextContext(model: ScenarioModel): String {
         model.states[currentState]?.let {
             if (!it.noContext) {
                 currentContext = it.path.toString()
@@ -38,7 +45,24 @@ class DialogContext: Serializable {
         return currentContext
     }
 
+    fun saveToTransitionHistory(state: String) {
+        ensureDeserialized()
+        while (transitionHistory.size >= TRANSITION_HISTORY_SIZE_LIMIT) {
+            transitionHistory.removeFirst()
+        }
+        transitionHistory.add(state)
+    }
+
+    private fun ensureDeserialized() {
+        // may be null after java deserialization
+        @Suppress("SENSELESS_COMPARISON")
+        if (transitionHistory == null) {
+            transitionHistory = ArrayDeque<String>(TRANSITION_HISTORY_SIZE_LIMIT).apply { add(currentState) }
+        }
+    }
+
     companion object {
         private const val serialVersionUID = -9180292787182200322L
+        private const val TRANSITION_HISTORY_SIZE_LIMIT: Int = 50
     }
 }
