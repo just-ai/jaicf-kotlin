2863636fd6b04d7610f9a935e93d5f6fc9f35760
Nikita Voloshin <n.voloshin@just-ai.com>
2021-08-09 17:19:49 +0300

    Wrap HttpBotRequest#stream to BufferedInputStream.
    * BufferedInputStream supports mark and reset
    * User mark + reset in receiveText()

--- core/src/main/kotlin/com/justai/jaicf/channel/http/HttpBotRequest.kt
+++ core/src/main/kotlin/com/justai/jaicf/channel/http/HttpBotRequest.kt
@@ -12,18 +12,24 @@ import java.nio.charset.Charset
  * @property parameters HTTP query parameters
  * @property requestMetadata optional metadata for request processing
  */
-data class HttpBotRequest(
-    val stream: InputStream,
+class HttpBotRequest(
+    stream: InputStream,
     val headers: Map<String, List<String>> = mapOf(),
     val parameters: Map<String, List<String>> = mapOf(),
     val requestMetadata: String? = null
 ) {
+    val stream = stream.buffered()
 
-    fun receiveText(charset: Charset = Charset.forName("UTF-8")) = stream.bufferedReader(charset).readText()
+    fun receiveText(charset: Charset = Charset.forName("UTF-8")) = stream.runAndReset { bufferedReader(charset).readText() }
 
     fun firstHeader(name: String) = headers[name]?.first()
 
     fun firstParameter(name: String) = parameters[name]?.first()
+}
+
+fun <R> InputStream.runAndReset(action: InputStream.() -> R): R {
+    mark(0)
+    return action().also { reset() }
 }
 
 fun String.asHttpBotRequest(requestMetadata: String? = null) = HttpBotRequest(
