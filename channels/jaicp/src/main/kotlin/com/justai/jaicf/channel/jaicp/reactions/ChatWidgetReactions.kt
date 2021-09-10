48f72bc6933cce59d33995e40ade361cae18a61e
Protasov Vitalii <veptechno@gmail.com>
2021-09-10 18:36:12 +0300

    chatwidget: add audio reaction

--- channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/reactions/ChatWidgetReactions.kt
+++ channels/jaicp/src/main/kotlin/com/justai/jaicf/channel/jaicp/reactions/ChatWidgetReactions.kt
@@ -1,10 +1,12 @@
 package com.justai.jaicf.channel.jaicp.reactions
 
 import com.justai.jaicf.channel.jaicp.JaicpLiveChatProvider
+import com.justai.jaicf.channel.jaicp.dto.AudioReply
 import com.justai.jaicf.channel.jaicp.dto.Button
 import com.justai.jaicf.channel.jaicp.dto.ButtonsReply
 import com.justai.jaicf.channel.jaicp.dto.CarouselReply
 import com.justai.jaicf.channel.jaicp.dto.ImageReply
+import com.justai.jaicf.logging.AudioReaction
 import com.justai.jaicf.logging.ButtonsReaction
 import com.justai.jaicf.logging.CarouselReaction
 import com.justai.jaicf.logging.ImageReaction
@@ -29,6 +31,11 @@ class ChatWidgetReactions(
     fun image(imageUrl: String, caption: String? = null): ImageReaction {
         replies.add(ImageReply(imageUrl, caption, currentState))
         return ImageReaction.create(imageUrl)
+    }
+
+    override fun audio(url: String): AudioReaction {
+        replies.add(AudioReply(url, currentState))
+        return AudioReaction.create(url)
     }
 
     fun button(text: String, @PathValue transition: String? = null): ButtonsReaction =
