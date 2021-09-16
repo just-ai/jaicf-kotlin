336e27a08c3109a9ec9122f31fccace8e1041266
Protasov Vitalii <veptechno@gmail.com>
2021-09-16 08:43:27 +0300

    plugin: support String expressions by PathValue

--- core/src/main/kotlin/com/justai/jaicf/plugin/PathValue.kt
+++ core/src/main/kotlin/com/justai/jaicf/plugin/PathValue.kt
@@ -4,13 +4,18 @@ package com.justai.jaicf.plugin
  * This annotation indicates to the plugin that a StatePath will be passed to the annotated parameter or receiver.
  * This allows you to use the plugin's features, such as state navigation and inspections. To use it,
  * you need to annotate a parameter of a function or a primary constructor that takes a state name.
- * Also, you can annotate a receiver type of extensions function.
+ * Also, you can annotate a receiver type of extensions function or a String expression inside a function.
  *
- * Example:
+ * Examples:
  * ```
- * fun @PathValue String.and(@PathValue that: String)
+ * fun @PathValue String.and(@PathValue that: String) {}
+ *
+ * fun example() {
+ *     val a = @PathValue "../a"
+ * }
+ *
  * ```
  */
-@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.TYPE)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.TYPE, AnnotationTarget.EXPRESSION)
 @Retention(AnnotationRetention.SOURCE)
 annotation class PathValue()
