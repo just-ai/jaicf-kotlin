0f819bdcee5ef53836b9fe942dbfea113bb852ae
Nikita Voloshin <n.voloshin@just-ai.com>
2021-10-01 12:53:59 +0300

    Add TypeToken

--- activators/caila/src/main/kotlin/com/justai/jaicf/activator/caila/CailaTypeToken.kt
+++ activators/caila/src/main/kotlin/com/justai/jaicf/activator/caila/CailaTypeToken.kt
@@ -3,5 +3,7 @@ package com.justai.jaicf.activator.caila
 import com.justai.jaicf.generic.ActivatorTypeToken
 
 typealias CailaTypeToken = ActivatorTypeToken<CailaIntentActivatorContext>
+typealias CailaEntityTypeToken = ActivatorTypeToken<CailaEntityActivatorContext>
 
 val caila: CailaTypeToken = ActivatorTypeToken()
+val cailaEntity: CailaEntityTypeToken = ActivatorTypeToken()
