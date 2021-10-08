91e51966c997dd8c0c56dfe1df29cbbd83e9727e
Nikita Voloshin <n.voloshin@just-ai.com>
2021-10-08 14:06:01 +0300

    Make onlyIf additive

--- core/src/main/kotlin/com/justai/jaicf/model/activation/ActivationRule.kt
+++ core/src/main/kotlin/com/justai/jaicf/model/activation/ActivationRule.kt
@@ -7,7 +7,8 @@ import com.justai.jaicf.context.BotContext
 interface ActivationRule {
 
     /**
-     * Tells whether resulting [ActivatorContext] created by this rule is able to activate a scenario state
+     * Tells whether resulting [ActivatorContext] created by this rule is able to activate a scenario state.
+     * This operation is short-circuit
      *
      * @param botContext current context of the bot
      * @param request current request
@@ -17,7 +18,7 @@ interface ActivationRule {
     fun isOnlyIf(botContext: BotContext, request: BotRequest, activator: ActivatorContext): Boolean
 
     /**
-     * Sets the given [predicate] as a post-match condition for this rule,
+     * Adds the given [predicate] as a post-match condition for this rule,
      * meaning that [ActivatorContext] created by this rule will be passed to scenario
      * only if the given predicate returs true.
      *
@@ -37,13 +38,14 @@ interface ActivationRule {
  * Base implementation of [ActivationRule] that provides support of [ActivationRule.onlyIf]
  */
 abstract class ActivationRuleAdapter : ActivationRule {
-    private var onlyIf: ActivationRule.OnlyIfContext.() -> Boolean = { true }
+    private var onlyIf: MutableList<ActivationRule.OnlyIfContext.() -> Boolean> = mutableListOf()
 
     override fun isOnlyIf(botContext: BotContext, request: BotRequest, activator: ActivatorContext): Boolean {
-        return onlyIf.invoke(ActivationRule.OnlyIfContext(botContext, request, activator))
+        val context = ActivationRule.OnlyIfContext(botContext, request, activator)
+        return onlyIf.all { it.invoke(context) }
     }
 
     override fun onlyIf(predicate: ActivationRule.OnlyIfContext.() -> Boolean) {
-        onlyIf = predicate
+        onlyIf.add(predicate)
     }
 }
