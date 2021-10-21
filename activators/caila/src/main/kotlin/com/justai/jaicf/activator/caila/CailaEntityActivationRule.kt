db9d4b4e3931dfbfef093bf3cf124abdbc019be6
Nikita Voloshin <n.voloshin@just-ai.com>
2021-10-21 19:49:32 +0300

    Fix CailaEntityActivationRule.kt after merge onlyIf

--- activators/caila/src/main/kotlin/com/justai/jaicf/activator/caila/CailaEntityActivationRule.kt
+++ activators/caila/src/main/kotlin/com/justai/jaicf/activator/caila/CailaEntityActivationRule.kt
@@ -2,8 +2,9 @@ package com.justai.jaicf.activator.caila
 
 import com.justai.jaicf.builder.ActivationRulesBuilder
 import com.justai.jaicf.model.activation.ActivationRule
+import com.justai.jaicf.model.activation.ActivationRuleAdapter
 
-open class CailaEntityActivationRule(val matches: (CailaEntityActivatorContext) -> Boolean) : ActivationRule
+open class CailaEntityActivationRule(val matches: (CailaEntityActivatorContext) -> Boolean) : ActivationRuleAdapter()
 open class CailaEntityByNameActivationRule(val entity: String) : CailaEntityActivationRule({ it.entity == entity })
 
 fun ActivationRulesBuilder.cailaEntity(entity: String) = rule(CailaEntityByNameActivationRule(entity))
