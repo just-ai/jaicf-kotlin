4c0e39fcf7967ad8e5c5bf5ca894aa22124a06ab
Veronika Akhapkina <v.akhapkina@just-ai.com>
2024-02-20 09:23:56 +0300

    fix: added new fields to Transition class

--- core/src/main/kotlin/com/justai/jaicf/model/transition/Transition.kt
+++ core/src/main/kotlin/com/justai/jaicf/model/transition/Transition.kt
@@ -5,5 +5,7 @@ import com.justai.jaicf.model.activation.ActivationRule
 data class Transition(
     val fromState: String,
     val toState: String,
-    val rule: ActivationRule
+    val rule: ActivationRule,
+    val modal: Boolean,
+    val noContext: Boolean
 )
