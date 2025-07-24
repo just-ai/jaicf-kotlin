00a6ace1ca215edcdf2699fa71dbd97af96270df
AlexMcGil <a.medvedev@just-ai.com>
2025-07-24 14:12:50 +0300

    remove old  versions

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,22 +1,11 @@
 object Version {
     // Kotlin
     const val kotlin = "2.0.20"
-    const val stdLib = "2.0.20"
-    const val reflect = "2.0.20"
 
     // Libraries
-    const val jackson = "2.18.2"
-    const val slf4j = "1.7.30"
     const val jUnit = "5.13.1"
-    const val jetty = "9.4.3.v20170317"
 
-    const val ktor = "1.5.1"
     const val serializationRuntime = "1.0.1"
-    const val coroutinesCore = "1.4.2"
-    const val tomcatServletApi = "6.0.53"
-    const val logbackGelfAppender = "1.5"
-    const val mockk = "1.10.2"
-    const val okHttp = "4.12.0"
 }
 
 infix fun String.version(versionProvider: Version.() -> String) = "$this:${versionProvider(Version)}"
